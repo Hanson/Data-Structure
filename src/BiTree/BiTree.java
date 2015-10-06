2cdf48834f6218861f7d4f2c3c14af66bd0d635d
HanSon <h@hanc.cc>
2015-10-06 11:17:00 +0800

    增加遍历方法

--- src/BiTree/BiTree.java
+++ src/BiTree/BiTree.java
@@ -1,5 +1,8 @@
 package BiTree;
 
+import Queue.QueueList.LinkQueue;
+import Stack.StackList.LinkStack;
+
 /**
  * Created by HanSon on 2015/7/20.
  * GitHub: http://github.com/hanccc
@@ -7,7 +10,7 @@ package BiTree;
  * Resume: http://hanccc.github.io
  */
 public class BiTree {
-    private  BiTreeNode root;
+    public BiTreeNode root;
     private static int index = 0;
 
     public BiTree(){
@@ -27,42 +30,117 @@ public class BiTree {
     }
 
     //先根遍历二叉树基本操作的非递归算法
-    public void preRootTraverse(){
+    public void preRootTraverse() throws Exception {
+        BiTreeNode T = root;
+        if(T!=null){
+            LinkStack S = new LinkStack();
+            S.push(T);
+            while (!S.isEmpty()){
+                T = (BiTreeNode) S.pop();
+                System.out.print(T.getData());
+                while (T != null){
+                    if (T.getLchild()!=null)
+                        System.out.print(T.getLchild().getData());
+                    if (T.getRchild()!=null)
+                        S.push(T.getRchild());
+                    T = T.getLchild();
+                }
+            }
 
+        }
     }
 
     //先根遍历二叉树基本操作的递归算法
     public void preRootTraverse(BiTreeNode T){
         if (T != null){
-            System.out.println(T.getData());
+            System.out.print(T.getData());
             preRootTraverse(T.getLchild());
             preRootTraverse(T.getRchild());
         }
     }
 
     //中根遍历二叉树基本操作的非递归算法
-    public void inRootTraverse(){
-
+    public void inRootTraverse() throws Exception {
+        BiTreeNode T = root;
+        if(T != null){
+            LinkStack S = new LinkStack();
+            S.push(T);
+            while (!S.isEmpty()){
+                while (S.peek()!=null)
+                    S.push(((BiTreeNode) S.peek()).getLchild());
+                S.pop();
+                if (!S.isEmpty()){
+                    T = (BiTreeNode) S.pop();
+                    System.out.print(T.getData());
+                    S.push(T.getRchild());
+                }
+            }
+        }
     }
 
     //中根遍历二叉树基本操作的递归算法
     public void inRootTraverse(BiTreeNode T){
-
+        if(T != null){
+            inRootTraverse(T.getLchild());
+            System.out.print(T.getData());
+            inRootTraverse(T.getRchild());
+        }
     }
 
     //后根遍历二叉树基本操作的非递归算法
-    public void postRootTraverse(){
-
+    public void postRootTraverse() throws Exception {
+        BiTreeNode T = root;
+        if(T != null){
+            LinkStack S = new LinkStack();
+            S.push(T);
+            Boolean flag;
+            BiTreeNode p = null;
+            while (!S.isEmpty()){
+                while (S.peek() != null)
+                    S.push(((BiTreeNode) S.peek()).getLchild());
+                S.pop();
+                while (!S.isEmpty()){
+                    T = (BiTreeNode) S.peek();
+                    if (T.getRchild() == null || T.getRchild() == p){
+                        System.out.print(T.getData());
+                        S.pop();
+                        p = T;
+                        flag = true;
+                    }else{
+                        S.push(T.getRchild());
+                        flag = false;
+                    }
+                    if (!flag)
+                        break;
+                }
+            }
+        }
     }
 
     //后根遍历二叉树基本操作的递归算法
     public void postRootTraverse(BiTreeNode T){
-
+        if(T!= null){
+            postRootTraverse(T.getLchild());
+            postRootTraverse(T.getRchild());
+            System.out.print(T.getData());
+        }
     }
 
     //层次遍历二叉树基本操作的算法（自左向右）
-    public void levelTraverse(){
-
+    public void levelTraverse() throws Exception {
+        BiTreeNode T = root;
+        if (T != null){
+            LinkQueue L = new LinkQueue();
+            L.offer(T);
+            while (!L.isEmpty()){
+                T = (BiTreeNode) L.poll();
+                System.out.print(T.getData());
+                if (T.getLchild() != null)
+                    L.offer(T.getLchild());
+                if(T.getRchild() != null)
+                    L.offer(T.getRchild());
+            }
+        }
     }
 
     public BiTreeNode getRoot() {
